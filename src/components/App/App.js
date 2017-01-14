4f38c83104e7e8758f9f15631286c3b038bad6ef
Raman <raman.lapei@gmail.com>
2017-01-14 16:01:24 +0300

    anthony_first

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './App.css';
+import Header from '../header/header';
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {test: 'foo'};
+	}
+
+	render() {
+		return (
+			<div className={styles.app}>
+				<Header param={111} />
+				bar
+			</div>
+		);
+	}
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,8 @@
+import './header.css';
+import React from 'react';
+
+export default class Header extends React.Component {
+    render(){
+        return <header>Header</header>
+    }
+}
