81c189d045068413c55cdc1646002e835ead8514
anthony <solidolic@gmail.com>
2017-01-18 15:39:51 +0400

    routing

---
+++ src/components/admin/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Admin extends React.Component {
+    render(){
+        return (
+            <div className='row'>
+                <div className='col-md-12'>Раздел /admin</div>
+            </div>
+        )
+    }
+}
